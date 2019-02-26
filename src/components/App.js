85209f93907b7d9bec27448a347adce65346cbdd
jtr2809 <j.torres.rojas@outlook.cl>
2019-02-25 22:50:16 -0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return(
+        <div>Wena los cabros</div>   
+        )
+    }
+}
+
+export default App;
